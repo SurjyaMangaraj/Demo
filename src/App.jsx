a29749aea1dd3b339575c6451a8814da740f968b
surjya12 <surjya.mangraj123@gmail.com>
2025-08-30 13:51:14 +0530

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <h1>HELLO SAI DATABASE</h1>
-    </>
-  )
+import Header from "./Header"
+function apple(){
+  return(
+    <div>
+         <Header/>
+        
+         <h1>Hello</h1>
+        
+    </div>
+  
+  ) 
 }
 
-export default App
+export default apple
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+    <div>
+        <h4>This is header</h4>
+    </div>
+    )
+}
+
+export default Header
